e5aabca170fbd179196603dab949fa64fdce5456
rosemarym2 <rosemarymay91@gmail.com>
2021-10-24 20:17:44 +0100

    changes v.1.4

--- Functions.js
+++ Functions.js
@@ -1,64 +1,64 @@
 /*Task 1 Cinema Ticket*/
 
-// let age = 60;
+let age = 60;
 
-// if (age < 18){
-//     console.log("£8");
-// } else if (age < 60){
-//     console.log("£10.95");
-// } else if (age > 59){
-//     console.log("£7.50");
-// } else {
-//     console.log("£7.50");
-// };
+if (age < 18){
+    console.log("£8");
+} else if (age < 60){
+    console.log("£10.95");
+} else if (age > 59){
+    console.log("£7.50");
+} else {
+    console.log("£7.50");
+};
 
 
-// let ticketPrices = [
-//     "Child (Below 18) £8",
-//     "Adult (18+ £10.95",
-//     "Senior (60+) £7.50",
-// ];
-// console.log(ticketPrices)
+let ticketPrices = [
+    "Child (Below 18) £8",
+    "Adult (18+ £10.95",
+    "Senior (60+) £7.50",
+];
+console.log(ticketPrices)
 
 /*Task 2 Favourite Color*/
 
-// const favColor = (color, notfav) => {
-//     console.log(`My favourite colour is ${color} not ${notfav}.`)
-// }
+const favColor = (color, notfav) => {
+    console.log(`My favourite colour is ${color} not ${notfav}.`)
+}
 
-// favColor("red", "blue");
+favColor("red", "blue");
 
 /*Activity 1 Turn into arrow function*/
-// function factorial (n) {
-    // if ((n === 0) || (n ===1)) {
-    // return 1;
-    // } else {
-    // return (n * factorial (n-1));
-    // }
-    // }
-    // console.log(factorial(33));
+function factorial (n) {
+    if ((n === 0) || (n ===1)) {
+    return 1;
+    } else {
+    return (n * factorial (n-1));
+    }
+    }
+    console.log(factorial(33));
 
 /* Answer */
-// const factorial = (n) => {
-//         if ((n === 0) || (n ===1)) {
-//         return 1;
-//         } else {
-//         return (n * factorial (n-1));
-//         }
-//         }
-//         console.log(factorial(33));
+const factorial = (n) => {
+        if ((n === 0) || (n ===1)) {
+        return 1;
+        } else {
+        return (n * factorial (n-1));
+        }
+        }
+        console.log(factorial(33));
 
 /*Activity 2 Pizza*/
-// let orderCount = 0;
+let orderCount = 0;
 
-// const takeOrder = (topping1, topping2) => {
-// console.log(`Pizza with ${topping1} and ${topping2}`);
-// orderCount++;
-// }
+const takeOrder = (topping1, topping2) => {
+console.log(`Pizza with ${topping1} and ${topping2}`);
+orderCount++;
+}
 
-// takeOrder("pepporoni", "olives");
-// takeOrder("mushrooms", "peppers");
-// takeOrder("chicken", "extra cheese");
+takeOrder("pepporoni", "olives");
+takeOrder("mushrooms", "peppers");
+takeOrder("chicken", "extra cheese");
 
 /*Activity 3 Cash Machine
 Dispenses cash if your pin number is correct and your balance is equal to, or more than, the amount you're trying to withdraw!*/
@@ -69,14 +69,15 @@ let balance = 300;
 let currency = `£`;
 let total = 300;
 
-// const cashWithdrawal = (amount, accNum) => {
-// console.log(`Your cash withdrawal of ${currency}${amount.toFixed(2)} for ${accNum} is being processed`);
-// balance++;
-// }
+/*test code
+const cashWithdrawal = (amount, accNum) => {
+console.log(`Your cash withdrawal of ${currency}${amount.toFixed(2)} for ${accNum} is being processed`);
+balance++;
+}
 
-// cashWithdrawal (300, accNumber);
-// cashWithdrawal (360, 50449921);
-// cashWithdrawal (30, 50447921);
+cashWithdrawal (300, accNumber);
+cashWithdrawal (360, 50449921);
+cashWithdrawal (30, 50447921);*/
 
 atmWithdraw = (atmAmount, accNum) => {
     if (1234 == pinNum && total <= balance) {
@@ -93,26 +94,26 @@ atmWithdraw = (atmAmount, accNum) => {
 console.log(atmWithdraw());
 
 
-// const person = {
-//     name: `Rosie`,
-//     age: 30,
-//     songs:[
-//         "Baba O'Riley",
-//         "MMMBop",
-//         "The Chain"
-//     ]
-// };
-//  console.log(person.songs[0]);
+const person = {
+    name: `Rosie`,
+    age: 30,
+    songs:[
+        "Baba O'Riley",
+        "MMMBop",
+        "The Chain"
+    ]
+};
+ console.log(person.songs[0]);
 
 
-// sayHi(){
-//     if (name) {
-//         return `Hello my name is ${this.name}`;
-//     }
-// },
-//     console.log(person.this.name());
+sayHi(){
+    if (name) {
+        return `Hello my name is ${this.name}`;
+    }
+},
+    console.log(person.this.name());
 
-/*broken code below
+/*broken code alarm clock
 let day = 'Saturday';
 let alarm = '';
 
@@ -128,85 +129,94 @@ if(day == 'Saturday'|| day == 'Sunday'){
 
 /*Objects
 Activity 1*/
-// const person = {
-//     name: `Rosie`,
-//     age: 30,
-//     songs:[
-//         "Baba O'Riley",
-//         "MMMBop",
-//         "The Chain"
-//     ]
-// }, 
-// sayHi = () => {
-//     return `Hello my name is ${person.name}`;
-// };
+const person = {
+    name: `Rosie`,
+    age: 30,
+    songs:[
+        "Baba O'Riley",
+        "MMMBop",
+        "The Chain"
+    ]
+}, 
+sayHi = () => {
+    return `Hello my name is ${person.name}`;
+};
 
-// console.log(sayHi(person.name));
+console.log(sayHi(person.name));
 
 
 /*Activity 2*/
-// const pet = {
-//     petName: `Jasper`,
-//     typeOfPet: `Dog`,
-//     age: 12,
-//     colour: `brown`,
-// }
-// console.log(pet['petName']);
+const pet = {
+    petName: `Jasper`,
+    typeOfPet: `Dog`,
+    age: 12,
+    colour: `brown`,
+}
+console.log(pet['petName']);
 
-// eat = () => {
-//    return`Your pet ${pet.petName} is eating`;
-// },
-// drink = () => {
-//    return`Your pet ${pet.petName} is drinking`;
-// };
+eat = () => {
+   return`Your pet ${pet.petName} is eating`;
+},
+drink = () => {
+   return`Your pet ${pet.petName} is drinking`;
+};
 
-// console.log(eat());
-// console.log(drink());
+console.log(eat());
+console.log(drink());
 
-/*Activity 3 cafe*/
+/*Activity 3 cafe broken code
 const coffeeShop = {
     branch: `200 Degrees Nottingham`,
     currency: `£`,
     drinks:[
-        [`Americano`, `Espresso`, `Latte`, `Flat White`, `Hot Chocolate`],
-        [2.25, 2.40, 2.35, 3.00],
+        [`Americano`, 2.25],
+        [`Espresso`, 2.25],
+        [`Latte`, 2.40],
+        [`Flat White`, 2.35],
+        [`Hot Chocolate`, 3.00],
     ],
     food: [
-        [`Chicken Salad`, `Cheese and Tomato Sandwich`, `Croissant`, `BLT sandwich`, `Cake Slice`] 
-        [4.50, 4.55, 1.20, 4.95, 3.50],
+        [`Chicken Salad`, 4.50], 
+        [`Cheese and Tomato Sandwich`, 4.55],
+        [`Croissant`, 1.20],
+        [`BLT sandwich`, 4.95],
+        [`Cake Slice`, 3.50], 
     ],
-},
+}
 
-// let order = '';
-// let cost = 0;
-// for (let i = 0; i < drinks.length; i++){
-//     for(let j = 0; j < this.food.length; j++){
-//         if(food[i] === this.food[j]){
-//             cost += this.food[j + 1]
-//             order = order + ` = ${food[i]} £${this.food[j + 1]} = `
-//         }    
-// };
+let order = '';
+let cost = 0;
+for (let i = 0; i < drinks.length; i++){
+    for(let j = 0; j < this.food.length; j++){
+        if(food[i] === this.food[j]){
+            cost += this.food[j + 1]
+            order = order + ` = ${food[i]} £${this.food[j + 1]} = `
+        }    
+};
 
-// foodOrdered = () =>{
-//         return console.log (`You ordered:${this.food[i][j]} and ${this.drinks[i][j]}. The cost for this order
-//         will be ${this.food[i][j] + this.drinks[i][j]}`) 
-//        }
+foodOrdered = () =>{
+        return `Your food order is:${this.food[i][j]} and ${this.drinks[i][j]}. The total cost for this order ${this.food[i][j] + this.drinks[0][1]}`
+       }
+drinksordered = () =>{
+    return `Your drinks order is: ${this.drinks[i][j]} and ${this.drinks}`
+}
     
-// coffeeShop (foodOrdered());
+console.log(foodOrdered())
+console.log(drinksOrdered())
 
 
 
 
-// let order = '';
-// let cost = 0;
-// for (let i = 0; i < drinks.length; i++){
-//     console.log(this.drinks[i])     
-// };
+let order = '';
+let cost = 0;
+for (let i = 0; i < drinks.length; i++){
+    console.log(this.drinks[i])     
+};
 
-// totalOrder = () => {
-//     return `Your order is ${this.food[i].currency[].toFixed(2)}`
-// }
-// console.log.coffeeShop(totalOrder())
+totalOrder = () => {
+    return `Your order is ${this.food[i].currency[].toFixed(2)}`
+}
+console.log.coffeeShop(totalOrder())*/
 
 /*Price List
 drinks:
@@ -223,17 +233,51 @@ food:
     Cake Slice - £3.50
 ],*/
 
+/*let orders() {
+    drinksOrders: 
+    foodOrders: 
+}
 
+function totalOrder(){
+    return console.log (`Your order is ${this.food[0].currency[0]}`);
+}
+coffeeShop.totalOrdered()
+coffeeShop(this.drinks),
+coffeeShop(this.food),
+console.log(coffeeShop.drinks[0][1]);*/
 
-// let orders() {
-//     drinksOrders: 
-//     foodOrders: 
-// }
+/*correct code with help*/
+let coffeeShop = {
+    branch: 'Costa',
+    drinks: ['Coffee', 2.50, 'Tea', 1.50, 'Hot Chocolate', 3.00],
+    food: ['Croissant', 1.50, 'Sandwich', 2.00],
+    currency: `£`,
+    drinksOrdered(drink){
+        let order = ''
+        let cost = 0
+        for(let i = 0; i < drink.length; i++){
+            for(let j = 0; j < this.drinks.length; j++){
+                if(drink[i] === this.drinks[j]){
+                    cost += this.drinks[j + 1]
+                    order = order + `${drink[i]} ${currency}${this.drinks[j + 1].toFixed(2)} + `
+                }
+            }
+        }
+    return `Drink ${order}\nCost  =  ${currency}${cost.toFixed(2)}\n`
+    },
+    foodOrdered(food){
+        let order = ''
+        let cost = 0
+        for(let i = 0; i < food.length; i++){
+            for(let j = 0; j < this.food.length; j++){
+                if(food[i] === this.food[j]){
+                    cost += this.food[j + 1]
+                    order = order + `${food[i]} ${currency}${this.food[j + 1].toFixed(2)} + `
+                }
+            }
+        }
+        return `Food  ${order}\nCost  =  ${currency}${cost.toFixed(2)}\n`
+    }
+}
 
-// function totalOrder(){
-//     return console.log (`Your order is ${this.food[0].currency[0]}`);
-// }
-// coffeeShop.totalOrdered()
-// coffeeShop(this.drinks),
-// coffeeShop(this.food),
-// console.log(coffeeShop.drinks[0][1]);
+console.log(coffeeShop.foodOrdered(['Croissant', 'Sandwich']), coffeeShop.drinksOrdered(['Coffee', 'Tea']))
